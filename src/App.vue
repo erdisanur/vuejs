3563af42981ffc79e566fe0c947ea22c5f7fa773
Erdisa Nur <erdisanoor@gmail.com>
2020-02-16 00:30:42 +0700

    'kedua'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,120 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <TopBar
+      @showUpdater="showUpdater = true"
+    />
+
+    <div id="wrapper">
+
+      <Updater
+        v-if="showUpdater"
+        :error="error"
+        @close="showUpdater = false"
+      />
+
+      <Loading
+        v-if="loadingOn"
+        :message="loadingMessage"
+        :showWheel="loadingWheel"
+      />
+
+      <router-view :key="this.viewKey"></router-view>
+
     </div>
-    <router-view />
+
+    <Foot />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import bus from './bus';
+  import Foot from './components/Foot';
+  import Loading from './components/Loading';
+  import Updater from './components/Updater';
+  import TopBar from './components/TopBar';
 
-#nav {
-  padding: 30px;
+  export default {
+    name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    data () {
+      return {
+        error: '',
+        loadingOn: true,
+        loadingMessage: 'Loading WP Vue',
+        loadingWheel: true,
+        showUpdater: false,
+        viewKey: 0,
+      }
+    },
 
-    &.router-link-exact-active {
-      color: #42b983;
+    created: function () {
+
+      this.assignEndpointFromURL();
+
+      bus.$on('toggleLoading', (status = true) => {
+
+        if(typeof status === 'string') {
+          this.loadingOn = true;
+          this.showWheel = false;
+          this.loadingMessage = status;
+          return;
+        }
+
+        this.loadingMessage = 'Loading...';
+        this.loadingWheel = true;
+        this.loadingOn = status;
+      });
+
+      bus.$on('bumpViewKey', (loadingMessage) => {
+        bus.$emit('toggleLoading', loadingMessage);
+        this.viewKey = this.viewKey + 1;
+      });
+
+      bus.$on('showUpdater', (errorMessage) => {
+        this.showUpdater = true;
+        this.error = errorMessage;
+      });
+
+      bus.$on('clearError', () => {
+        this.error = '';
+      });
+    },
+
+    methods: {
+      assignEndpointFromURL: function () {
+        let endpoint = this.getQueryString('endpoint');
+
+        if(!endpoint) return;
+
+        this.$store.commit('updateEndpoint', endpoint);
+      }
+    },
+
+    components: {
+      Foot,
+      Loading,
+      Updater,
+      TopBar
     }
   }
-}
+</script>
+
+<style lang="scss">
+  @import "assets/scss/_base.scss";
+
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  #wrapper {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 1rem;
+
+    @include media($small) {
+      padding: 2rem;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from "vue";
+import VueAnalytics from "vue-analytics";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
+import utils from "./mixins/utils";
 import store from "./store";
 
-Vue.config.productionTip = false;
+Vue.mixin(utils);
+
+//-- Initialize Google Analytics tracking.
+Vue.use(VueAnalytics, {
+  id: GA_TRACKING_ID,
+  router
+});
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
