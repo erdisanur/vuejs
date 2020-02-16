275bf06f5751af2c38b0d21693a6f8772458d1f1
Erdisa Nur <erdisanoor@gmail.com>
2020-02-16 14:21:13 +0700

    'keduabelas'

--- vue.config.js
+++ vue.config.js
@@ -18,12 +18,6 @@ module.exports = {
       ]
     }
   },
-  pages: {
-    app: {
-      entry: path.resolve(__dirname, "./src/main.js"),
-      template: path.resolve(__dirname, "./public/index.html")
-    }
-  },
   configureWebpack: {
     plugins: [new webpack.DefinePlugin(CONSTANTS)]
   }
